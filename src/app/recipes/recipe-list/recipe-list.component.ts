1f49fa0be8d8ada7dc6a1c804bf1c1e35c59cbc0
siddharthUCD <siddharth.dinesh@ucdconnect.ie>
2022-03-09 00:59:15 +0000

    Before recipe list

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'This is a test recipe da fool!', 'https://cdn.loveandlemons.com/wp-content/uploads/2020/03/pantry-recipes-1-900x1024.jpg')
+  ];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
